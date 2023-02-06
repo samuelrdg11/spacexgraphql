97b5239a79b965ead3c54408247f4ebd8af64fdf
Samuel <samuelrdg11@gmail.com>
2023-02-06 17:19:40 -0500

    Modificaciones en archivo css y cliente

--- src/Client.js
+++ src/Client.js
@@ -20,20 +20,17 @@ function Launches() {
     if (error) return <p>Error : {error.message}</p>;
 
     return (
-
-        <div className="App">
             <ul>
             {(data.launchesPast.map(({ id, mission_name, details, launch_date_local }) =>
-                <div key={id}>
+                <div key={id} className='container d-flex justify-content-center'>
+                    <div className='card mx-auto mb-2'>
                     <h4>{mission_name}</h4>
                     <small>{details}</small>
                     <p>{launch_date_local}</p>
+                    </div>
                 </div>
-            )
-            )
-            }
+            ))}
             </ul>
-        </div>
     )
 }
 
